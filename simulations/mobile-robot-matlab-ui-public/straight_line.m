1577897a5aeacd2df82559936e9f5c8d096b07dd
Will <william.gwynne@student.manchester.ac.uk>
2021-03-02 17:44:17 +0000

    Update straight_line.m

--- simulations/mobile-robot-matlab-ui-public/straight_line.m
+++ simulations/mobile-robot-matlab-ui-public/straight_line.m
@@ -1,89 +1,90 @@
-clc, clear all
-restoredefaultpath
-addpath(genpath(pwd))
+function my_alg = example_control(my_alg, robot)
+% This function implements velocity controllers for both wheels 
+% and applies the desired setpoints for a specified amount of time.
+%
+% Mohamed Mustafa, August 2020
+% -------------------------------------------------------------------------
+%
+% Reading data from sensors (if present on the robot)
+%    my_alg('right encoder') - right encoder velocity
+%    my_alg('left encoder')  - left encoder velocity
+%    my_alg('reflectance')   - reflectance sensor output value
+%    my_alg('reflectance raw')   - reflectance sensor raw output values
+%    my_alg('sonar')         - sonar measured distance (m)
+% 
+% Sending controls to actuators (if present on the robot)
+%    my_alg('right motor')   - sets the right motor input signal (pwm or angular velocity)
+%    my_alg('left motor')    - sets the left motor input signal (pwm or angular velocity)
+%    my_alg('servo motor')   - sets the servomotor angle (radians)
+% -------------------------------------------------------------------------
 
-
-% Create Robot
-R1 = RobotClass('json_fname', 'puzzle_bot_0002.json');
-% Uncomment below for using the real robot (it's simulation otherwise)
-% R1.connect('192.168.1.1');
-
-% Create World
-W = WorldClass('fname', 'world_empty.json');
-
-% Total duration and sampling time parameters
-TotalTime = 3;
-t_sampling = 0.02;
-
-t_start = tic;
-t_loop = tic;
-
-%% Initialise data
-
-% Initialise motor angular velocity controllers
-control_right = MotorControl();
-control_left = MotorControl();
-
-% Controller setpoints for right and left wheel
-wR_set = 7;
-wL_set = 7;
-
-% Wheel angular velocities from the encoders
-wR = 0;
-wL = 0;
-
-% Vectors for saving input/output data
-wR_set_all = [];
-wL_set_all = [];
-wR_all = [];
-wL_all = [];
-
-while toc(t_start)<TotalTime
+if my_alg('is_first_time')
+    %% Setup initial parameters here
     
-    dt = toc(t_loop);
+    my_alg('dc_motor_signal_mode') = 'voltage_pwm';     % change if necessary to 'omega_setpoint'
     
-    if(dt>=t_sampling)              % execute code when desired sampling time is reached
-        t_loop = tic;
+    % Initialise wheel angular velocity contollers
+    my_alg('wR_set') = 7;
+    my_alg('wL_set') = 7;
+    
+    my_alg('control_right') = MotorControl();
+    my_alg('control_left') = MotorControl();
+    
+    % Initialise vectors for saving velocity data
+    my_alg('wR_all') = [];
+    my_alg('wL_all') = [];
         
-        %% Add your code here %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % uR,uL - control input pwm signals [-1,1]
-        % wR,wL - wheel output angular velocities
-
-        %% Replace with your PI controllers here %%%%%%%%%%%%%%%%
-        % Right wheel controller (replace with your controller)        
-        uR = control_right.Control(wR_set,wR,dt);
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-        % Left wheel controller (replace with your controller)
-        uL = control_left.Control(wL_set,wL,dt);
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        
-        % Save data for ploting
-        wR_set_all = [wR_set_all wR_set];
-        wL_set_all = [wL_set_all wL_set];
-        wR_all = [wR_all wR];
-        wL_all = [wL_all wL];
-                
-        %% End %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-        
-        %% Do not edit this section
-        % Update robot in this order: actuators, pose (in simulation), sensors
-        actuator_signals = {'right motor', uR, 'left motor', uL};
-        sensor_readings = R1.update(dt, W, 'kinematics', 'voltage_pwm', actuator_signals{:});
-        
-        % Update encoder velocity readings
-        wR = sensor_readings('right encoder');
-        wL = sensor_readings('left encoder');        
-    end
-
-     pause(0.001)
+    % Initialise time parameters
+    my_alg('t_sampling') = 0.03;
+    my_alg('t_loop') = tic;
+    my_alg('t_finish') = 3;
 end
 
-% Plot wheel angular velocities
-plot(wR_all);
-hold on;
-plot(wL_all);
-plot(wR_set_all);
+%% Loop code runs here
 
+time = toc(my_alg('tic'));      % Get time since start of session
 
+if time < my_alg('t_finish')    % Check for algorithm finish time
+    
+    dt = toc(my_alg('t_loop'));
+    
+    if dt>my_alg('t_sampling')  % execute code when desired sampling time is reached
+        my_alg('t_loop') = tic;
+
+        %% Add your loop code here (replace with your controller)%%%%%%%%%
+        % Right wheel controller %%%%%%%%%%%%%%%%%%%%
+        uR = my_alg('control_right').Control(my_alg('wR_set'),my_alg('right encoder'),dt);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        % Left wheel controller %%%%%%%%%%%%%%%%%%%%%
+        uL = my_alg('control_left').Control(my_alg('wL_set'),my_alg('left encoder'),dt);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        % Apply pwm signal
+        my_alg('right motor') = uR;
+        my_alg('left motor') = uL;
+
+        % Save data for ploting
+        my_alg('wR_all') = [my_alg('wR_all') my_alg('right encoder')];
+        my_alg('wL_all') = [my_alg('wL_all') my_alg('left encoder')];
+        %% End %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   end
+
+else
+%     %% Finish algorithm and plot results
+%     
+%     % Stop motors
+%     my_alg('right motor') = 0;
+%     my_alg('left motor') = 0;
+%     % Stop session
+%     my_alg('is_done') = true;
+%     
+%     % Plot saved velocities for right and left wheel
+%     figure(2);
+%     plot(my_alg('wR_all'));
+%     hold on
+%     plot(my_alg('wL_all'));
+
+end
+
+return
