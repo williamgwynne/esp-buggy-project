e3803a3ae4c01ee34dcd4ad417b19a530faea1d2
Will <william.gwynne@student.manchester.ac.uk>
2021-03-10 21:14:20 +0000

    Further edits to controlled-square stuff

--- simulations/mobile-robot-matlab-ui-public/controlled_square.m
+++ simulations/mobile-robot-matlab-ui-public/controlled_square.m
@@ -54,9 +54,9 @@ if my_alg('is_first_time')
     %PID coefficients for line-speed control
     my_alg('forwardspeed') = 0;
     my_alg('distance') = 0;
-    my_alg('kp_distance') = 0.1 %20 %36;
-    my_alg('ki_distance') = 0 %100 %0.5 %600;
-    my_alg('kd_distance') = 0 %20 %4 %0.45;
+    my_alg('kp_distance') = 0.1; %20 %36;
+    my_alg('ki_distance') = 0; %100 %0.5 %600;
+    my_alg('kd_distance') = 0; %20 %4 %0.45;
     my_alg('errordistance_sum') = 0;
     my_alg('errordistance_prev') = 0;
     
@@ -75,48 +75,49 @@ if time < my_alg('t_finish')    % Check for algorithm finish time
         
     if dt>my_alg('t_sampling')  % execute code when desired sampling time is reached
         my_alg('t_loop') = tic;
-        
-        if ((my_alg('forwardspeed') == 0) && (my_alg('distance')==1) && (my_alg('cornercount')<8)) %sets the angle to turn by
-            my_alg('cornercount') = my_alg('cornercount')+1;
-            corner=my_alg('cornercount')
-            if(my_alg('cornercount')==4)
-                my_alg('phi') = my_alg('pi'); %complete a full turn at the 4th corner
-            else
-                my_alg('phi') = my_alg('pi')/2;
-            end
-        end
-        
-        if (my_alg('phi') == 0)
-            %speed adjustments
-            linearVelocity_left = my_alg('left encoder') * 0.05; %v=wr
-            linearVelocity_right = my_alg('right encoder') * 0.05; %v=wr
-            averageVelocity = (linearVelocity_left + linearVelocity_right)/2;
-            my_alg('distance') = (averageVelocity*dt) + my_alg('distance');
+        if (my_alg('phi') < 0.01)
+            if ((my_alg('forwardspeed') < 0.01) && (my_alg('distance')>0.99) && (my_alg('cornercount')<8)) %sets the angle to turn by
+                my_alg('cornercount') = my_alg('cornercount')+1;
+                corner=my_alg('cornercount') %trace, delete afterwards
+                if(my_alg('cornercount')==4)
+                    my_alg('phi') = my_alg('pi'); %complete a full turn at the 4th corner
+                else
+                    my_alg('phi') = my_alg('pi')/2;
+                end
+                my_alg('distance') = 0;
+            else        
+                %speed adjustments
+                linearVelocity_left = my_alg('left encoder') * 0.05; %v=wr
+                linearVelocity_right = my_alg('right encoder') * 0.05; %v=wr
+                averageVelocity = (linearVelocity_left + linearVelocity_right)/2;
+                my_alg('distance') = (averageVelocity*dt) + my_alg('distance');
 
-            errordistance = 1 - my_alg('distance');
-            my_alg('errordistance_sum') = my_alg('errordistance_sum') + errordistance;
-            my_alg('forwardspeed') = (0 + (errordistance * my_alg('kp_distance') + my_alg('ki_distance') * my_alg('errordistance_sum')*dt + my_alg('kd_distance') * (errordistance-my_alg('errordistance_prev'))/dt))/dt; %0 is steady state speed
-            my_alg('errordistance_prev') = errordistance;
-            my_alg('wR_set') = my_alg('forwardspeed')/0.05; %-0.09*adjustmentangle)/0.05; %converting to angular speed
-            my_alg('wL_set') = my_alg('forwardspeed')/0.05; %+0.09*adjustmentangle)/0.05; 
-            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                my_alg('errordistance') = 1 - my_alg('distance');
+                my_alg('errordistance_sum') = my_alg('errordistance_sum') + my_alg('errordistance');
+                my_alg('forwardspeed') = (0 + (my_alg('errordistance') * my_alg('kp_distance') + my_alg('ki_distance') * my_alg('errordistance_sum')*dt + my_alg('kd_distance') * (my_alg('errordistance')-my_alg('errordistance_prev'))/dt))/dt; %0 is steady state speed
+                my_alg('errordistance_prev') = my_alg('errordistance');
+                my_alg('wR_set') = my_alg('forwardspeed')/0.05; %-0.09*adjustmentangle)/0.05; %converting to angular speed
+                my_alg('wL_set') = my_alg('forwardspeed')/0.05; %+0.09*adjustmentangle)/0.05; 
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            end
         else
-            %controller for turning 90 degrees, calculate error and
-            %converge on 0
-            %my_alg('wR_set') = +speed
-            %my_alg('wL_set') = -speed
+            %controller for turning, linearity will return when phi<0.01
+            rotationalspeed = 1;
+            my_alg('difftheta') = (-2*rotationalspeed)/my_alg('buggy_width');
+            my_alg('wR_set') = -rotationalspeed/0.05; %opposite wheel direction for clockwise motion
+            my_alg('wR_set') = rotationalspeed/0.05;
         end
         
         
         % Right wheel controller %%%%%%%%%%%%%%%%%%%%
-        errorspeedright = my_alg('wR_set')-my_alg('right encoder')
+        errorspeedright = my_alg('wR_set')-my_alg('right encoder');
         my_alg('errorspeedright_sum') = errorspeedright + my_alg('errorspeedright_sum');
         uR = (my_alg('wR_set') + (errorspeedright * my_alg('kp_speed') + my_alg('ki_speed') * my_alg('errorspeedright_sum')*dt + my_alg('kd_speed') * (errorspeedright-my_alg('errorspeedright_prev'))/dt))*my_alg('w2p_ratio');
         my_alg('errorspeedright_prev') = errorspeedright;
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         % Left wheel controller %%%%%%%%%%%%%%%%%%%%%
-        errorspeedleft = my_alg('wL_set')-my_alg('left encoder')
+        errorspeedleft = my_alg('wL_set')-my_alg('left encoder');
         my_alg('errorspeedleft_sum') = errorspeedleft+my_alg('errorspeedleft_sum');
         uL = (my_alg('wL_set') + (errorspeedleft * my_alg('kp_speed') + my_alg('ki_speed') * my_alg('errorspeedleft_sum')*dt + my_alg('kd_speed') * (errorspeedleft-my_alg('errorspeedleft_prev'))/dt))*my_alg('w2p_ratio');
         my_alg('errorspeedleft_prev') = errorspeedleft;
@@ -128,7 +129,7 @@ if time < my_alg('t_finish')    % Check for algorithm finish time
         
         
         % Save data for ploting
-        my_alg('wR_all') = [my_alg('wR_all') errordistance];
+        my_alg('wR_all') = [my_alg('wR_all') my_alg('errordistance')];
         my_alg('wL_all') = [my_alg('wL_all') my_alg('right encoder')];
         my_alg('distance_all') = [my_alg('distance_all') my_alg('distance')];
         %% End %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
