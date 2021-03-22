a0b4d88bd039641d34fdd302dfd874a18059fd65
Will <william.gwynne@student.manchester.ac.uk>
2021-03-22 12:52:02 +0000

    Completed task 1 for line & sonar sensors

--- simulations/mobile-robot-matlab-ui-public/TD2 Tasks/task1_sonar.m
+++ simulations/mobile-robot-matlab-ui-public/TD2 Tasks/task1_sonar.m
@@ -1,4 +1,4 @@
-function my_alg = task1(my_alg, robot)
+function my_alg = task1_sonar(my_alg, robot)
 % This function implements velocity controllers for both wheels 
 % and applies the desired setpoints for a specified amount of time.
 %
@@ -39,7 +39,9 @@ if my_alg('is_first_time')
     % Initialise time parameters
     my_alg('t_sampling') = 0.03;
     my_alg('t_loop') = tic;
-    my_alg('t_finish') = 15;
+    my_alg('t_finish') = 10;
+    my_alg('dt_total') = 0;
+    my_alg('dt_all') = [];
     
     %initialise kinematic variables
     my_alg('distance') = 0;
@@ -61,6 +63,8 @@ if my_alg('is_first_time')
     my_alg('errordistance_sum') = 0;
     my_alg('errordistance_prev') = 0;
     
+    my_alg('sonar_all') = [];
+    
 end
 
 %% Loop code runs here
@@ -73,41 +77,11 @@ if time < my_alg('t_finish')    % Check for algorithm finish time
     
     if dt>my_alg('t_sampling')  % execute code when desired sampling time is reached
         my_alg('t_loop') = tic;
-        if ((my_alg('distance') < 2.95) || (my_alg('distance') > 3.05))
-            linearVelocity_left = my_alg('left encoder') * 0.05; %v=wr
-            linearVelocity_right = my_alg('right encoder') * 0.05; %v=wr
-
-            my_alg('averageVelocity') = (linearVelocity_left + linearVelocity_right)/2;
-            my_alg('distance') = (my_alg('averageVelocity')*dt) + my_alg('distance');
-
-            %speed adjustments
-            errordistance = 3 - my_alg('distance'); %3 metres is set distance for straight line
-            my_alg('errordistance_sum') = my_alg('errordistance_sum') + errordistance;
-               if (my_alg('errordistance_sum')>3)
-                   my_alg('errordistance_sum')=3;
-               elseif (my_alg('errordistance_sum')<0)
-                   my_alg('errordistance_sum')=0;
-               end
-            forwardspeed = (0 + (errordistance * my_alg('kp_distance') + my_alg('ki_distance') * my_alg('errordistance_sum')*dt + my_alg('kd_distance') * (errordistance-my_alg('errordistance_prev'))/dt))/dt; %0 is steady state speed
-            my_alg('errordistance_prev') = errordistance;
-
-             if (-0.01 < forwardspeed < 0.01)
-                errorspeedright = 0
-                errorspeedleft = 0
-            end
-
-            my_alg('wR_set') = forwardspeed/0.05; %converting to angular speed
-            my_alg('wL_set') = forwardspeed/0.05;
-        else
-            my_alg('wR_set') = 0;
-            my_alg('wL_set') = 0;
-            my_alg('averageVelocity') = 0;
-            my_alg('errorspeedright_sum') = 0;
-            my_alg('errorspeedleft_sum') = 0;
-            my_alg('errorspeedright_prev') = 0;
-            my_alg('errorspeedleft_prev') = 0;
-        end
         
+        %speed adjustments
+        forwardspeed = 0.5;
+        my_alg('wR_set') = forwardspeed/0.05; %converting to angular speed
+        my_alg('wL_set') = forwardspeed/0.05;  
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         % Right wheel controller %%%%%%%%%%%%%%%%%%%%
@@ -127,6 +101,12 @@ if time < my_alg('t_finish')    % Check for algorithm finish time
         % Apply pwm signal
         my_alg('right motor') = uR;
         my_alg('left motor') = uL;
+        
+        %save data for plotting
+        my_alg('sonar_all') = [my_alg('sonar_all') my_alg('sonar')];
+        my_alg('dt_total') = my_alg('dt_total') + dt;
+        my_alg('dt_all') = [my_alg('dt_all') my_alg('dt_total')];
+        
         %% End %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    end
 
@@ -138,6 +118,13 @@ else
     my_alg('left motor') = 0;
     % Stop session
     my_alg('is_done') = true;
+    
+    figure(1);
+    plot(my_alg('dt_all'), my_alg('sonar_all'));
+    title('Sonar readings as buggy approaches wall');
+    xlabel('Time');
+    ylabel('Sonar reading');
+    hold on
 end
 
 return
