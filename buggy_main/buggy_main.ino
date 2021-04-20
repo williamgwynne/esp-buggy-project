2f999f5514945a68384c43c8810188d77a0e8062
Will <william.gwynne@student.manchester.ac.uk>
2021-04-20 02:47:08 +0100

    Calculating angular velocity from encoder readings

--- buggy_main/buggy_main.ino
+++ buggy_main/buggy_main.ino
@@ -9,7 +9,7 @@ ESP32Encoder EncR, EncL;
 double wR_set = 0, wL_set = 0;
 double distance = 0;
 double w_p_ratio = 901/12500;
-double toc_last = 0;
+double toc_last = 0.0;
 
 //PID coefficients for motor control......................................................................
 double errorspeedright_prev = 0, errorspeedleft_prev = 0, errorspeedright_sum = 0, errorspeedleft_sum = 0;
@@ -44,14 +44,17 @@ void setup()
 
 void loop()
 {
-  double toc = micros()/1000000;
+  double toc = micros()/1000000.0;
   double dt = toc - toc_last;
   toc_last = toc;
-  int piss = int(EncR.getCount());
-  Serial.print(piss);
-  Serial.print("\n");
-  float EncR_Speed = EncR.getCount()
+  
+  float EncR_Speed = (EncR.getCount()/1632.67)/dt; //1632.67 counts per revolution according to manufacturer
+  float EncL_Speed = (EncR.getCount()/1632.67)/dt; //1632.67 counts per revolution according to manufacturer
   EncR.clearCount();
+  EncL.clearCount(); //force this into a class, EncL_Speed should be a member access function instead, e.g. use EncL.getSpeed()
+
+  
+  Serial.println(EncR_Speed, 5);
 //  double forwardSpeed = (EncR.Get_Speed()+EncL.Get_speed())/2;
   
   if (distance < 3) {     //stops running after 3 metres, to imitate TD1 task 5
