fdcaad04b3c2a44bd602707a8bc4911a7bb0a938
Will <william.gwynne@student.manchester.ac.uk>
2021-04-30 21:12:04 +0100

    Updated library

--- td4/buggy.h
+++ td4/buggy.h
@@ -3,6 +3,9 @@
 #include "Ticker.h"
 #include "ESP32Encoder.h"
 
+// Callback function header
+void callback(char* topic, byte* payload, unsigned int length);
+
 class Sonar
 {
 private:
@@ -11,21 +14,69 @@ private:
 	int signalPin;
 public:
 	Sonar(int sigpin) : signalPin(sigpin) {}
-	float getDist() {
+	float getDist() 
+	{
 		
 	}
 };
 
-class Servo
+class Servo_
 {
 private:
 public:
+  Servo_(int pin)
+  {
+    //pinMode(pin, OUTPUT);
+    
+  }
+  void setAngle(int angle)
+  {
+    
+  }
 };
 
-class Encoder
+class Motor : private MotorDriver, private ESP32Encoder //includes code for both motor and encoder
 {
 private:
+const float kp = 0.3;
+const float ki = 0.001;
+const float kd = 0.012;
+float w_set;
+int encoderA, encoderB;
+ESP32Encoder encoder;
+float dt;
+float encoder_lastCount, errorSpeed_sum, errorSpeed_prev;
 public:
+  Motor (int pwmPin, int pwmChannel, int pinA, int pinB, int encA, int encB, int dt_ms) : encoderA(encA), encoderB(encB), dt(dt_ms/1000.0)
+  //dt_ms is the delay of the ticker
+  {
+    SetMotorType(DC_MOTOR);
+    SetBaseFreq(5000);
+    DriverSetup(pwmPin, pwmChannel, pinA, pinB);
+    attachFullQuad(encoderA, encoderB);
+    encoder_lastCount = 0;
+    errorSpeed_sum = 0;
+    errorSpeed_prev = 0;
+    w_set = 0;
+  }
+  void adjustSpeed() //ISR, if possible attach to a ticker within this class and move to private
+  {
+    float encoder_speed = ((getCount()-encoder_lastCount)/1632.67)/dt; //1632.67 counts per revolution according to manufacturer
+    float errorSpeed = w_set - encoder_speed;
+    errorSpeed_sum += errorSpeed;
+    float u = (w_set + (errorSpeed * kp) + (ki * errorSpeed_sum * dt) + ((kd*(errorSpeed-errorSpeed_prev))/dt));
+    errorSpeed_prev = errorSpeed;
+    encoder_lastCount = getCount();
+    MotorWrite(-u/2.37); //max speed = 2.37 rads/s
+  }
+  void setAngularSpeed(float angularSpeed)
+  {
+    w_set = angularSpeed;
+  }
+  void setLinearSpeed(float linearSpeed)
+  {
+    w_set = linearSpeed/0.05; //w=v/r
+  }
 };
 
 class Buggy
--- td4/td4.ino
+++ td4/td4.ino
@@ -3,14 +3,39 @@
 #include "buggy.h"
 #include "Ticker.h"
 
-Sonar fuck(14);
+const int dt_millis = 100;
+
+Motor right_motor(4, 0, 15, 18, 34, 36, dt_millis);
+Motor left_motor(2, 0, 12, 0, 39, 35, dt_millis);
+Ticker updateMotorSpeeds; //could not be called inside class, callback() foes not exist as in Mbed
+
+void adjustSpeeds() 
+{
+  right_motor.adjustSpeed();
+  left_motor.adjustSpeed();
+}
 
 void setup()
 {
   Serial.begin(9600);
+  updateMotorSpeeds.attach_ms(dt_millis, &adjustSpeeds);
+  right_motor.setAngularSpeed(1); //max speed =~2.37 rads/s
+  left_motor.setAngularSpeed(1);
 }
 
 void loop()
 {
-  sonar.getDist();
+  for (float i=0; i<2.37; i+=0.01)
+  {
+    right_motor.setAngularSpeed(i);
+    left_motor.setAngularSpeed(i);
+    delay(100);
+  }
+  for (float i=2.37; i>0; i-=0.01)
+  {
+    right_motor.setAngularSpeed(i);
+    left_motor.setAngularSpeed(i);
+    delay(100);
+  }
+//  sonar.getDist();
 }
